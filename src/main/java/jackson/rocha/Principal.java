ff9457a32df919374caba4e5ba648b7ee166ae9c
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-21 08:15:59 -0300

    Interfaces e Injeção de dependências

--- src/main/java/jackson/rocha/Principal.java
+++ src/main/java/jackson/rocha/Principal.java
@@ -2,6 +2,7 @@ package jackson.rocha;
 
 import jackson.rocha.modelo.Fazenda;
 import jackson.rocha.modelo.Industria;
+import jackson.rocha.modelo.ParceiroFinanceiro;
 import jackson.rocha.servico.ServicoFinanciamento;
 
 public class Principal {
@@ -9,8 +10,10 @@ public class Principal {
         var servicoFinanciamento = new ServicoFinanciamento();
         var fazenda = new Fazenda("Fazenda Dona Benedita", 5_000_000, 5);
         var industria = new Industria("Alimentos da Vovó", 900_000, true);
+        var parceiro = new ParceiroFinanceiro("Capital ABC", 2_000_000);
+
 
         servicoFinanciamento.solicitarFinanciamento(fazenda, 600_000);
-        servicoFinanciamento.solicitarFinanciamento(industria, 600_000);
+        // servicoFinanciamento.solicitarFinanciamento(industria, 600_000);
     }
 }
