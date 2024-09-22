6dcaf99b4104629fe379cc98745f8de2929c1d03
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-22 19:07:07 -0300

    Refatorando usando extends e implements na mesma classe

--- src/main/java/jackson/rocha/Principal.java
+++ src/main/java/jackson/rocha/Principal.java
@@ -1,6 +1,7 @@
 package jackson.rocha;
 
 import jackson.rocha.modelo.Fazenda;
+import jackson.rocha.modelo.Funcionario;
 import jackson.rocha.modelo.Industria;
 import jackson.rocha.modelo.ParceiroFinanceiro;
 import jackson.rocha.servico.ServicoFinanciamento;
@@ -11,9 +12,11 @@ public class Principal {
         var fazenda = new Fazenda("Fazenda Dona Benedita", 5_000_000, 5);
         var industria = new Industria("Alimentos da Vovó", 900_000, true);
         var parceiro = new ParceiroFinanceiro("Capital ABC", 2_000_000);
+        var funcionario = new Funcionario("João da Silva", 18_000);
 
 
+        servicoFinanciamento.solicitarFinanciamento(funcionario, 90_000);
         servicoFinanciamento.solicitarFinanciamento(fazenda, 600_000);
-        // servicoFinanciamento.solicitarFinanciamento(industria, 600_000);
+        servicoFinanciamento.solicitarFinanciamento(industria, 500_000);
     }
 }
