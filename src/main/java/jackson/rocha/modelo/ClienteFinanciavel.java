6185a8ac989a5322e6e5b24a6ea92fa95d0633c1
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-22 19:46:21 -0300

    Metodos privado nas interfaces

--- src/main/java/jackson/rocha/modelo/ClienteFinanciavel.java
+++ src/main/java/jackson/rocha/modelo/ClienteFinanciavel.java
@@ -5,12 +5,20 @@ public interface ClienteFinanciavel {
     double calcularLimiteAprovado();
 
     default double calcularJuros(double valorSolicitado) {
-        if (valorSolicitado <= 100_000) {
+        if (isFinanciamentoPequenoValor(valorSolicitado)) {
             return 1.0;
-        } else if (valorSolicitado <= 100_000_000) {
+        } else if (isFinanciamentoGrandeValor(valorSolicitado)) {
             return 1.5;
         } else {
             return 2.0;
         }
+    }
+
+    private static boolean isFinanciamentoGrandeValor(double valorSolicitado) {
+        return valorSolicitado <= 100_000_000;
+    }
+
+    private static boolean isFinanciamentoPequenoValor(double valorSolicitado) {
+        return valorSolicitado <= 100_000;
     }
 }
