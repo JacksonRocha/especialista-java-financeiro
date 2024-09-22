f188b594f68c63d78a338bb9116ab5a5b3ff14aa
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-22 19:24:41 -0300

    Refatorando usando extends e implements na mesma classe

--- src/main/java/jackson/rocha/modelo/ClienteFinanciavel.java
+++ src/main/java/jackson/rocha/modelo/ClienteFinanciavel.java
@@ -4,4 +4,13 @@ public interface ClienteFinanciavel {
 
     double calcularLimiteAprovado();
 
+    default double calcularJuros(double valorSolicitado) {
+        if (valorSolicitado <= 100_000) {
+            return 1.0;
+        } else if (valorSolicitado <= 100_000_000) {
+            return 1.5;
+        } else {
+            return 2.0;
+        }
+    }
 }
